a6da2eb308bb74955ad49a4ec82c1e012ebc6cd7
Dmitriy Martynov <dm346@mail.ru>
2024-12-03 11:37:48 +0300

    Create localstorage.vue

---
+++ localstorage.vue
@@ -0,0 +1,8 @@
+
+  <div v-if="state.email !== undefined && state.token !== undefined">
+    <div class="mt-20"> {{ state.email }}</div>
+
+import { useStorage } from '@vueuse/core'
+const state = useStorage('vue-token', { email: undefined, token: undefined })
+  state.value.email = enteremail.value
+  state.value.token = res.access_token
